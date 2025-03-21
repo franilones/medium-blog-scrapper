85ed320658120dd111cb1f9d8413cbf3a1d5b1ab
franilones <franolmosdev@gmail.com>
2025-03-21 11:50:29 +0100

    First commit with dependencies.

---
+++ src/main/java/com/franilones/medium_blog_scrapper/MediumBlogScrapperApiApplication.java
@@ -0,0 +1,13 @@
+package com.franilones.medium_blog_scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumBlogScrapperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediumBlogScrapperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franilones/medium_blog_scrapper/MediumBlogScrapperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franilones.medium_blog_scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumBlogScrapperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
